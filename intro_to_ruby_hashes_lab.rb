995b34dc9179758177a0e6a85a7a7a7e02fb46d6
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-17 16:38:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hash[:key] = value
-  return hash
+  hash[:key] = "value"
+  puts hash
   # return a hash that includes the key and value parameters passed into this method
 end
 
