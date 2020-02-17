731958a05d9461ec142540d235c729ee0a41d0a8
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-17 16:41:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # hash{:key => "value"}
-  # if
-  # else
-  #   nil
-  # end
+  hash = {:key => "value"}
+
+  end
   
   # return the correct value using the hash and key parameters
 end
